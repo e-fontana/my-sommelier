d1f142c51cb36e2ada6a642d40d956d14c3c6867
Eduardo Lazarini Fontana <contato@eduardofontana.dev>
2026-02-17 16:15:42 -0300

    feat: adds log for the request

--- src/alexa/alexa.module.ts
+++ src/alexa/alexa.module.ts
@@ -1,9 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { skillAdapter } from './adapters/alexa.adapter';
+import { AlexaLoggerMiddleware } from './middlewares/alexa.middleware';
 
 @Module({})
 export class AlexaModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(...skillAdapter.getRequestHandlers()).forRoutes('alexa');
+    consumer
+      .apply(AlexaLoggerMiddleware, ...skillAdapter.getRequestHandlers())
+      .forRoutes('alexa');
   }
 }
--- src/alexa/middlewares/alexa.middleware.ts
+++ src/alexa/middlewares/alexa.middleware.ts
@@ -1,42 +1,15 @@
-// alexa.middleware.ts
-import { ExpressAdapter } from 'ask-sdk-express-adapter';
-import { RequestHandler } from 'express';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
 
-export function createAlexaMiddleware(adapter: ExpressAdapter): RequestHandler {
-  const middlewares = adapter.getRequestHandlers();
+@Injectable()
+export class AlexaLoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger(AlexaLoggerMiddleware.name);
 
-  return async (req, res, _): Promise<void> => {
-    try {
-      await new Promise<void>((resolve, reject) => {
-        const first = middlewares[0];
-        const second = middlewares[1];
+  use(req: Request, _res: Response, next: NextFunction): void {
+    this.logger.log(`Headers recebidos:`);
 
-        if (!first) {
-          return reject(new Error('Alexa middleware not found'));
-        }
+    this.logger.debug(JSON.stringify(req.headers, null, 2));
 
-        first(req, res, (err?: unknown) => {
-          if (err instanceof Error) {
-            return reject(err);
-          }
-
-          if (second) {
-            second(req, res, () => resolve());
-          } else {
-            resolve();
-          }
-        });
-      });
-    } catch (error: unknown) {
-      const message =
-        error instanceof Error ? error.message : 'Unknown verification error';
-
-      res.status(400).json({
-        statusCode: 400,
-        error: 'Bad Request',
-        message: 'Request verification failed',
-        details: message,
-      });
-    }
-  };
+    next();
+  }
 }
