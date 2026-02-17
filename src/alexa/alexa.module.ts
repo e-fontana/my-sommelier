bd4b719c35a45e915f7fca98778301439f010968
Eduardo Lazarini Fontana <contato@eduardofontana.dev>
2026-02-17 19:51:56 -0300

    chore: removed unused logs

--- src/alexa/alexa.module.ts
+++ src/alexa/alexa.module.ts
@@ -1,12 +1,9 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { skillAdapter } from './adapters/alexa.adapter';
-import { AlexaLoggerMiddleware } from './middlewares/alexa.middleware';
 
 @Module({})
 export class AlexaModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(AlexaLoggerMiddleware, ...skillAdapter.getRequestHandlers())
-      .forRoutes('alexa');
+    consumer.apply(...skillAdapter.getRequestHandlers()).forRoutes('alexa');
   }
 }
