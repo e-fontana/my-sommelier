486014521b1b4bc84ef52209ff33d4d3700c5a25
Eduardo Lazarini Fontana <contato@eduardofontana.dev>
2026-02-17 16:20:08 -0300

    chore: adiciona logs

--- src/alexa/intents/launch-request.ts
+++ src/alexa/intents/launch-request.ts
@@ -1,10 +1,18 @@
+import { Logger } from '@nestjs/common';
 import { RequestHandler } from 'ask-sdk-core';
+
+const logger = new Logger('LaunchRequestHandler');
 
 export const LaunchRequestHandler: RequestHandler = {
   canHandle(handlerInput) {
+    logger.log(
+      'Verificando se LaunchRequestHandler pode lidar com a requisição...',
+    );
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
   },
   handle(handlerInput) {
+    logger.log('LaunchRequest foi acionado!');
+
     return handlerInput.responseBuilder
       .speak('Bem-vindo ao meu sommelier.')
       .reprompt('Como posso ajudar?')
